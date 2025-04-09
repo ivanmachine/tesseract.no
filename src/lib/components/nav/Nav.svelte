127bbbb648c4af67cd0523c966f8b404f6e0cf11
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2025-04-09 05:16:50 +0200

    Trying to add some cool particles

--- src/lib/components/nav/Nav.svelte
+++ src/lib/components/nav/Nav.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
   import LanguageSwitch from "./LanguageSwitch.svelte";
+  import NavLink from "./NavLink.svelte";
 </script>
 
 <nav class="flex w-full justify-between items-center">
   <a class="title" href="/home">Tesseract</a>
 
   <div class="flex ml-auto gap-8 center links">
-    <a href="/home">{$_("nav_home")}</a>
-    <a href="/services">{$_("nav_services")}</a>
-    <a href="/projects">{$_("nav_projects")}</a>
-    <a href="/about-us">{$_("nav_about")}</a>
-    <a href="/articles">{$_("nav_articles")}</a>
-    <a href="/contact">{$_("nav_contact")}</a>
+    <NavLink text={$_("nav_home")} href="/home" />
+    <NavLink text={$_("nav_services")} href="/services" />
+    <NavLink text={$_("nav_projects")} href="/projects" />
+    <NavLink text={$_("nav_articles")} href="/articles" />
+    <NavLink text={$_("nav_about")} href="/about-us" />
+    <NavLink text={$_("nav_contact")} href="/contact" />
     <LanguageSwitch />
   </div>
 </nav>
 
 <style>
   nav {
+    position: fixed;
+    top: 0;
     padding: 1rem 2rem;
   }
-  .links > a:hover {
-    text-decoration: underline;
-  }
-
   .title {
     font-size: 2rem;
     font-weight: 700;
---
+++ src/lib/components/nav/NavLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let text: string;
+  export let href: string;
+</script>
+
+<a {href}>{text}</a>
+
+<style>
+  a:hover {
+    text-decoration: underline;
+    background-color: rgba(173, 216, 230, 0.1); /* Light blue background */
+    box-shadow: 0 0 50px rgba(173, 216, 230, 0.5); /* Light blue glow */
+  }
+</style>
