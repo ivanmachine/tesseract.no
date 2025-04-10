1269f67dfeac9867418cac03ba966d31ad257be6
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2025-04-10 13:11:58 +0200

    Trying to add animations

--- src/lib/components/animation/Transition.svelte
+++ src/lib/components/animation/Transition.svelte
@@ -33,7 +33,14 @@
     left: 0;
     width: 100vw;
     height: 100vh;
-    background-color: black;
+    clip-path: polygon(0 0, 100% 100%, 0 100%);
+    background: rgb(2, 0, 36);
+    background: linear-gradient(
+      45deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(9, 9, 121, 1) 35%,
+      rgba(0, 212, 255, 1) 100%
+    );
     opacity: 0;
   }
 </style>
